b3b639078e66ab8656015cf9a9b8667bbceb9fb7
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-05-31 20:13:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-interview-kit-sample1-reactjs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-ef4k3o)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>Insert Text</h1>
+      <p>Read the description to start solving the problem. 💯</p>
+    </div>
+  );
+}
